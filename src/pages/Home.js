a9b9e511280df47406d6bdb124ff4ce4cd13cb39
Redaboukir <Redaboy71@gmail.com>
2025-03-06 09:38:31 +0000

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur Tadlaoui Shop</h1>
+      <p>Découvrez nos plus beaux tableaux décoratifs !</p>
+    </div>
+  );
+};
+
+export default Home;
